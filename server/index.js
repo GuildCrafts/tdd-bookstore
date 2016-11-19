404b7c25e1d481034afd76446de7dde6d992ca51
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-11-18 17:51:51 -0800

    bases for test driven bookstore

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+// const bookData = require('./test/books.json')
 const server = express()
 
 server.set('port', process.env.PORT || '3000')
+
+mongoose.Promise = global.Promise
+
+mongoose.createConnection(
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URI ||
+  'mongodb://127.0.0.1:27017/hamHarm' )
+
+
 
 server.get('/ping', (request, response, next) => {
   response.send('pong')
