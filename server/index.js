8049e55207df44510519e7d6f1f5f2de73992334
Harman Singh <harmanlearns@gmail.com>
2016-11-21 15:06:46 -0800

    WIP getting books method

--- server/index.js
+++ server/index.js
@@ -4,20 +4,17 @@ const server = express()
 const http = require('http')
 const Book = require('../models/book.js')
 const errorResponse = require( './errorResponse')
-const {libraryPromise} = require('../models/book.js')
-const bodyParser = require('body-parser')
 
-var debug = require('debug')('src:server')
+const bodyParser = require('body-parser')
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: false }));
 
+var debug = require('debug')('src:server')
+
+// const {libraryPromise} = require('../models/book.js')
 
 mongoose.Promise = global.Promise
-// const connection = 'mongodb://127.0.0.1:27017/test'
 const connection = 'mongodb://localhost:27017/test'
-
-// server.on('error', onError)
-// server.on('listening', onListening)
 
 const port = ( process.env.PORT || '3000')
 server.set('port', port)
@@ -25,58 +22,27 @@ server.set('port', port)
 mongoose.connect(connection)
   .then(() => console.log('connection successful'))
   .catch((error) => console.error(error))
-  // .then(() => server.listen(port))
-  // .catch( error => next(error))
-
-  function onError(error) {
-    if (error.syscall !== 'listen') {
-      throw error
-    }
-
-    var bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges')
-        process.exit(1)
-        break
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use')
-        process.exit(1)
-        break
-      default:
-        throw error
-    }
-  }
-
-
-// server.set('port', process.env.PORT || '3000')
 
 server.get('/ping', (request, response, next) => {
   response.send('pong')
 })
 
 server.post('/api/books', (request, response, next) => {
-  // console.log('body',request.body)
-  // console.log('Book',Book)
   Book.create(request.body)
-    // .then(book => {
-    //   if(request.body.title) {
-    //     return response.status(201).json(book)
-    //   }else {
-    //     var message = 'title cannot be blank'
-    //     console.log(message)
-    //     return response.status(400).response.send(message)
-    //   }})
-    // .catch( error => next(error))
-
-    .then(reex =>  response.json(reex))
-    .catch( errorResponse( response ))
+    .then(book => {
+      if(request.body.title) {
+        book.genres.sort()
+        return response.status(201).json(book)
+      }else {
+        response.body = {error:{message: "title cannot be blank"}}
+        return response.status(400).json(response.body)
+      }})
+    .catch( error => next(error))
 })
 
+server.get('api/books', (request, response, next) => {
+  
+})
 server.post('/api/test/reset-db', (request, response, next) => {
   // mongoimport --db test --collection <books> --type json --file ../test/books.json --jsonArray
   response.send('db reset')
