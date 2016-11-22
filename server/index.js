96180ea21479eed65650f17a7c1573e511054384
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-11-22 13:05:38 -0800

    work in progress

--- server/index.js
+++ server/index.js
@@ -8,58 +8,82 @@ const BOOKS = require('../test/books.json')
 // const jquery = require('jquery')
 // const $ = require('jquery')(require("jsdom").jsdom().parentWindow)
 
-const bodyParser = require('body-parser')
+const bodyParser = require( 'body-parser' )
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: false }));
 
-var debug = require('debug')('src:server')
+var debug = require( 'debug' )( 'src:server' )
 
 // const {libraryPromise} = require('../models/book.js')
 
 mongoose.Promise = global.Promise
 const connection = 'mongodb://localhost:27017/test'
 
-const port = ( process.env.PORT || '3000')
-server.set('port', port)
+const port = ( process.env.PORT || '3000' )
+server.set( 'port', port )
 
-mongoose.connect(connection)
-  .then(() => console.log('connection successful'))
-  .catch((error) => console.error(error))
+mongoose.connect( connection )
+  .then(() => console.log( 'connection successful' ))
+  .catch(( error ) => console.error( error ))
 
-server.get('/ping', (request, response, next) => {
+server.get('/ping', ( request, response, next ) => {
   response.send('pong')
 })
 
-server.post('/api/books', (request, response, next) => {
-  Book.create(request.body)
-    .then(book => {
-      if(request.body.title) {
+//Create book from query passed to it
+server.post( '/api/books', ( request, response, next ) => {
+  Book.create( request.body )
+    .then( book => {
+      if( request.body.title ) {
         book.genres.sort()
-        return response.status(201).json(book)
+        return response.status( 201 ).json( book )
       }else {
-        response.body = {error:{message: "title cannot be blank"}}
-        return response.status(400).json(response.body)
+        response.body = { error:{ message: "title cannot be blank" }}
+        return response.status( 400 ).json( response.body )
       }})
-    .catch( error => next(error))
+    .catch( error => next( error ))
 })
 
-server.get('/api/books/', (request, response, next) => {
+//Gets paginated booklist or returns books for specific author
+server.get( '/api/books/', ( request, response, next ) => {
 
-    if(request.query.author){
-      toTitleCase = (str) => {
-        return str.replace(/\w\S*/g, (txt) => {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    if( request.query.author ){
+      toTitleCase = ( str ) => {
+        return str.replace( /\w\S*/g, ( txt ) => { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() })
       }
-      const something = toTitleCase(request.query.author)
+      const something = toTitleCase( request.query.author )
       // let re = new RegEx(something, "g")
-      let re = new RegExp(something, 'i')
-      console.log("authorPerson: ", something);
+      let re = new RegExp( something, 'i' )
+      console.log( "authorPerson: ", something )
 
-      Book.find().where('author').regex(re).limit(3).exec()
-        .then( books => { const result = books.filter( book => {
+      Book.find().where( 'author' ).regex( re ).limit( 3 ).exec()
+        .then( books => { let result = books.filter( book => {
+          return book.title })
+          response.status( 200 ).json( result )})
+        .catch( error => next( error ))
+    }else if( request.query.title ) {
+      toTitleCase = ( str ) => {
+        return str.replace(/\w\S*/g, ( txt ) => { return txt.charAt(0).toUpperCase() + txt.substr( 1 ).toLowerCase() })
+      }
+      const something = toTitleCase( request.query.title )
+      // let re = new RegEx(something, "g")
+      let re = new RegExp( something, 'i' )
+      console.log( "authorPerson: ", something )
+
+      Book.find().where( 'title' ).regex( re ).limit( 3 ).exec()
+        .then( books => { let result = books.filter( book => {
           return book.title})
-          response.status(200).json(result)})
+          response.status(200).json( result )})
+        .catch( error => next( error ))
+    }else if(request.query.year) {
+      const number = request.query.year
+
+      Book.find( {year: number} ).limit(5).exec()
+        .then( books => { let result = books.filter( book => {
+          return book.year})
+          response.status(200).json( result )})
         .catch( error => next(error))
-    }else{
+    }else {
       Book.find().limit(10).exec()
         .then( books => response.status(200).json(books))
           // if(books.length == 10) =>
@@ -68,6 +92,8 @@ server.get('/api/books/', (request, response, next) => {
     }
 })
 
+
+//Resets the database
 server.post('/api/test/reset-db', (request, response, next) => {
   // mongoimport --db test --collection books --type json --file books.json --jsonArray
   response.send('db reset')
