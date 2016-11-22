b7818167dc95de0c4c02eeb2c0bf5f0926014439
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-11-21 17:49:59 -0800

    displays first 10 books, cannot pull books by author

--- server/index.js
+++ server/index.js
@@ -41,18 +41,36 @@ server.post('/api/books', (request, response, next) => {
       }})
     .catch( error => next(error))
 })
-const createBooks = (books) =>
-  Promise.all(books.map(createBook))
-const loadFixtureData = () => createBooks(BOOKS)
+
+// server.get('/api/books/', (request, response, next) => {
+//   Book.find().limit(10).exec()
+//     .then( books => response.status(200).json(books))
+//       // if(books.length == 10) =>
+//       // return response.status(201)
+//     .catch( error => next(error))
+//
+// })
 
 
-server.get('/api/books/', (request, response, next) => {
-  console.log(request.body.id)
-  Book.find().limit(10).exec()
-    .then( books => response.status(200).json(books))
-      // if(books.length == 10) =>
-      // return response.status(201)
-    .catch( error => next(error))
+server.get('/api/books', (request, response) => {
+  console.log("im here!!!");
+  function toTitleCase(str)
+  {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+  const author = toTitleCase(request.param('author'))
+
+  // Book.find().populate({ path: 'books', match: { author: { $gte: author } }, select: 'title'}).exec()
+  //   .then( books => response.status(200).json(books))
+  const result = BOOKS.filter( author => {
+    return "author" === author
+  })
+  console.log("author: ", result);
+  const books = result.filter( title => {
+    return "title" === result.title
+  })
+  console.log("bruh: ", books);
+
 })
 
 server.post('/api/test/reset-db', (request, response, next) => {
